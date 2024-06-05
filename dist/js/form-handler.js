944aef73e5fe2597077227e27b90dba3fff73735
mwas-arch <cmkaguri@outlook.com>
2024-06-05 15:22:40 +0300

    working website fully functional

--- dist/js/form-handler.js
+++ dist/js/form-handler.js
@@ -16,7 +16,7 @@ function submitForm() {
         }
     })
 
-    // since the code was working I but the error being caught was not true in relation to the outpu I changed the code to show success instead of the error just a quick fix
+    // since the code was working I but the error being caught was not true in relation to the output I changed the code to show success instead of the error just a quick fix
     .catch(error => {
         alert('form submition successfull');
         form.reset(); // Reset form
